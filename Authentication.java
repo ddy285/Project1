e218c031512582c2c3c0036441994dc951beb438
Danielle Young <ddy285@email.vccs.edu>
2018-01-21 21:13:26 -0500

    Fixed Errors

--- Authentication.java
+++ Authentication.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class Authentication{
-public static void main(Strings [] args){
+public static void main(String [] args){
 Scanner input = new Scanner (System.in);
-String username= "ddy285"
-string password= "123"
+String username= "ddy285";
+String password= "123";
 System.out.println("Username: ");
 String consoleUsername = input.nextLine();
 System.out.println("Password: ");
-String consolePassword = inpiut.nextLine();
+String consolePassword = input.nextLine();
 if(consoleUsername.equals(username) && consolePassword.equals(password)){
 System.out.println("Welcome " +username);
 }else{
