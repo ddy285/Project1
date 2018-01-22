3adca7084a2b3967295a3c1f702647394a1c9de8
Danielle Young <ddy285@email.vccs.edu>
2018-01-21 19:14:34 -0500

    init

---
+++ Authentication.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Authentication {
+	public static void main(String [] args) {
+	Scanner input = new Scanner (System.in);
+	String username = "ddy";
+	String password = "123";
+	System.out.println("Username: ");
+	String consoleUsername = input.nextLine();
+	System.out.println("Password ");
+	String consolePassword = input.nextLine();
+	if (consoleUsername.equals(username) && consolePassword.equals(password)){
+		System.out.println("Welcome " +username);
+	}
+	else{
+		System.out.println("Invalid username or password");
+		}
+	}
+}
